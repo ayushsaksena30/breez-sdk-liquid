b26247a857a86bb929d66a2f81db56b15436bef0
Ayush Saksena <asaksena100@gmail.com>
2025-09-10 03:41:34 +0530

    Merge main branch into yse-plugin-base-rebased

--- lib/core/src/test_utils/sdk.rs
+++ lib/core/src/test_utils/sdk.rs
@@ -68,7 +68,8 @@ pub(crate) async fn new_liquid_sdk_with_chain_services(
         new_sync_service(persister.clone(), recoverer.clone(), signer.clone())?;
     let sync_service = Arc::new(sync_service);
 
-    LiquidSdkBuilder::new(config, STAGING_BREEZSERVER_URL.into(), signer)?
+    let mut builder = LiquidSdkBuilder::new(config, STAGING_BREEZSERVER_URL.into(), signer)?;
+    builder
         .bitcoin_chain_service(bitcoin_chain_service)
         .liquid_chain_service(liquid_chain_service)
         .onchain_wallet(onchain_wallet)
